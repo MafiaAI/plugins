86c5a85dbfed2774023ffef41a011e8393a0e877
PANKAJ <107530377+MafiaOS@users.noreply.github.com>
2022-07-04 21:25:58 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,6 +1,6 @@
 
 # Thanks to @D3_krish
-# Porting in MafiaBot by @H1M4N5HU0P
+# Porting in MafiaBot by @MafiaOS
 
 import asyncio
 import random
@@ -28,16 +28,16 @@ pm_caption = "  __**🔥🔥𝐌𝐀𝐅𝐈𝐀 𝐁𝐎𝐓 𝐈𝐒 𝐀𝐋�
 
 pm_caption += f"**━━━━━━━━━━━━━━━━━━━━**\n\n"
 pm_caption += (
-    f"                 👑𝐌𝐀𝐒𝐓𝐄𝐑👑\n  **『😈[{DEFAULTUSER}](tg://user?id={mafia})😈』**\n\n"
+    f"  👑𝐌𝐀𝐒𝐓𝐄𝐑👑\n  **『😈[{DEFAULTUSER}](tg://user?id={mafia})😈』**\n\n"
 )
 pm_caption += f"┏━━━━━━━━━━━━━━━━━━━\n"
 pm_caption += f"┣•➳➠ `Telethon:` `{version.__version__}` \n"
 pm_caption += f"┣•➳➠ `Version:` `{mafiaversion}`\n"
 pm_caption += f"┣•➳➠ `Sudo:` `{sudou}`\n"
 pm_caption += f"┣•➳➠ `Channel:` [ᴊᴏɪɴ](https://t.me/mafiasupport)\n"
-pm_caption += f"┣•➳➠ `Creator:` [Himanshu](https://t.me/pankaj10161)\n"
+pm_caption += f"┣•➳➠ `Creator:` [PANKAJ](https://t.me/pankaj10161)\n"
 pm_caption += f"┗━━━━━━━━━━━━━━━━━━━\n"
-pm_caption += " [🔥REPO🔥](https://github.com/MafiaBotOP/MafiaBot) 🔹 [📜License📜](https://github.com/MafiaBotOP/MafiaBot/blob/main/LICENSE)"
+pm_caption += " [🔥REPO🔥](https://github.com/MafiaOS/Mafia) 🔹 [📜License📜](https://github.com/MafiaOS/Mafia/blob/main/LICENSE)"
 
 # @command(outgoing=True, pattern="^.alive$")
 @bot.on(admin_cmd(outgoing=True, pattern="alive$"))
